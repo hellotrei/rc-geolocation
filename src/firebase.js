4e01660fe0d10cd734fb47d68a32de8797ec55cc
Treido <treido.vendor.pasti@pegadaian.co.id>
2023-02-06 10:00:35 +0700

    add firebase

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,19 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: "AIzaSyBlg2RthTrPShVYl35MFhXocodx0Otl7f8",
+
+  authDomain: "cekresiindah-5a80b.firebaseapp.com",
+
+  databaseURL: "https://cekresiindah-5a80b-default-rtdb.firebaseio.com",
+
+  projectId: "cekresiindah-5a80b",
+
+  storageBucket: "cekresiindah-5a80b.appspot.com",
+
+  messagingSenderId: "737317532672",
+
+  appId: "1:737317532672:web:da979dbb26f4f2616654e5"
 };
 
 firebase.initializeApp(firebaseConfig);
